505e5246fb85cfaa69ec48db75b5359c22bd3d61
Shaun Warman <swarman@paypal.com>
2015-10-13 11:12:17 -0500

    Move static folder to webpack build folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 
 console.log("Listening on port: 3000");
 app.listen(process.env.PORT || 3000);
