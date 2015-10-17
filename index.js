f6c684d080ea1900ada99696fdea7317eb0eafda
Shaun Warman <swarman@paypal.com>
2015-10-17 00:52:04 -0500

    Add jade views for initial render

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var routes = require('./server/controllers');
-
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/client/templates');
+app.set('view engine', 'jade');
+
 app.use(express.static(__dirname + '/build'));
-app.use('/', routes);
+
+app.get('/home', function (req, res) {
+    res.render('index');    
+});
 
 console.log("Listening on port: 3000");
 app.listen(process.env.PORT || 3000);
